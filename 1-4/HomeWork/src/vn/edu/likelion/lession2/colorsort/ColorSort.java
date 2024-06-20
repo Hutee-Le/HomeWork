e6583c59243e0cf8025aa37aaf0a9a8ef6eed378
Minh Nhut <lenhut0929@gmail.com>
2024-06-20 16:08:16 +0700

    update lession02 1th

--- 1-4/HomeWork/src/vn/edu/likelion/lession2/colorsort/ColorSort.java
+++ 1-4/HomeWork/src/vn/edu/likelion/lession2/colorsort/ColorSort.java
@@ -1,11 +1,26 @@
 package vn.edu.likelion.lession2.colorsort;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 public class ColorSort {
+    private static List<String> getSort(List<String> colors) {
+        List<String> sortList = new ArrayList<>(colors);
+        int n = sortList.size();
+        for (int i = 0; i < n - 1;  i++){
+            for (int j = 0; j < n - i - 1; j++){
+                if (sortList.get(j).compareTo(sortList.get(j+1)) >0){
+                    String temp = sortList.get(j);
+                    sortList.set(j, sortList.get(j + 1));
+                    sortList.set(j+1, temp);
+                }
+            }
+        }
+        return sortList;
+    }
+
     public static void main(String[] args) {
-        ArrayList<String> colors = new ArrayList<>();
+        List<String> colors = new ArrayList<>();
         colors.add("Red");
         colors.add("Green");
         colors.add("Orange");
@@ -13,7 +28,7 @@ public class ColorSort {
         colors.add("Black");
 
         System.out.println("List before sort: " + colors);
-        Collections.sort(colors);
-        System.out.println("List after sort: " + colors);
+
+        System.out.println("List after sort: " + getSort(colors));
     }
 }

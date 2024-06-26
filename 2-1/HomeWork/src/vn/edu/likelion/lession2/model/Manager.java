89475bef669cc8511ca8aa52dc0a2dc007c74e48
Minh Nhut <lenhut0929@gmail.com>
2024-06-26 15:26:00 +0700

    update javadoc

--- 2-1/HomeWork/src/vn/edu/likelion/lession2/model/Manager.java
+++ 2-1/HomeWork/src/vn/edu/likelion/lession2/model/Manager.java
@@ -3,16 +3,23 @@ package vn.edu.likelion.lession2.model;
 public class Manager extends Employee{
     private double bonus;
 
+
     public Manager(String name, double salary, double bonus) {
         super(name, salary);
         this.bonus = bonus;
     }
 
+    /**
+     * @return salary of programmer
+     */
     @Override
     public double calculateSalary() {
         return getSalary() + bonus;
     }
 
+    /**
+     *  this method is display detail of manager
+     */
     @Override
     public void displayInfo() {
         System.out.println("Manager Name: " + getName());
--- 2-1/HomeWork/src/vn/edu/likelion/lession2/model/Programmer.java
+++ 2-1/HomeWork/src/vn/edu/likelion/lession2/model/Programmer.java
@@ -4,17 +4,30 @@ public class Programmer extends Employee{
     private int overtimeHour;
     private double hourlyRate;
 
+    /**
+     * this is constructor of class
+     * @param name
+     * @param salary
+     * @param overtimeHour
+     * @param hourlyRate
+     */
     public Programmer(String name, double salary, int overtimeHour, double hourlyRate) {
         super(name, salary);
         this.overtimeHour = overtimeHour;
         this.hourlyRate = hourlyRate;
     }
 
+    /**
+     * @return salary of programmer
+     */
     @Override
     public double calculateSalary() {
         return getSalary() + (overtimeHour * hourlyRate);
     }
 
+    /**
+     * this method is display detail
+     */
     @Override
     public void displayInfo() {
         System.out.println("Programmer Name: " + getName());
